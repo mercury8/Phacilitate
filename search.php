ba8dc252f1b38c237b6fb8b2063fe60e87b9a9ad
Ishmael Bennett <mercurybennett@hotmail.com>
2023-09-26 20:55:02 +0100

    search fix 2

--- search.php
+++ search.php
@@ -15,17 +15,17 @@ global $wp_query;
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<div class="search_main">
-				<div class="small-container">
+				<div class="container">
 					<div class="search_main_wrapper">
 						
-						<div class="search_main_form">
+						<div class="search_main_form m-auto">
 							<?php get_search_form(); ?>
-							<a href="javascript:void(0)" class="search-close">
+							<!-- <a href="javascript:void(0)" class="search-close">
 								<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<circle cx="12" cy="12" r="12" fill="white" fill-opacity="0.8"/>
 									<path d="M16.2008 8.64678L15.3548 7.80078L12.0008 11.1548L8.64678 7.80078L7.80078 8.64678L11.1548 12.0008L7.80078 15.3548L8.64678 16.2008L12.0008 12.8468L15.3548 16.2008L16.2008 15.3548L12.8468 12.0008L16.2008 8.64678Z" fill="#434343" fill-opacity="0.4"/>
 								</svg>
-							</a>
+							</a> -->
 						</div>
 
 						<?php // if ( have_posts() ) : ?>
@@ -44,16 +44,26 @@ global $wp_query;
 										<?php 
 										$postQuery = new WP_Query(array(
 											'nopaging' => true,
-											'post_type'        => 'post',
+											'post_type'        =>  'post',
 											'post_status'    => 'publish',
 											'fields' => 'ids',
+											// 'content_type' =>'insight',
+											
 											's' => get_search_query(),
+											// 'date_query' => array(
+											// 	array(
+											// 		'after'     => 'January 1st, 2022',
+											// 		'before'    => 'January 1st, 2024',
+											// 		'inclusive' => true,
+											// 	),
+											// ),
 										));
 										$postList = $postQuery->posts;
 										// $maxPages = $postQuery->max_num_pages;
 										// echo '<pre>'; print_r( $postList ); echo '</pre>';
 										if( count( $postList ) > 0 ):
 										?>
+										
 										<div class="search_count"><?php echo $postQuery->found_posts.' results found'; ?></div>
 										<h6 class="heading h6">Explore</h6>
 										<?php
@@ -71,9 +81,20 @@ global $wp_query;
 										$postQuery = new WP_Query(array(
 											'nopaging' => true,
 											'post_type'        => 'insight',
+											
 											'post_status'    => 'publish',
 											'fields' => 'ids',
+											// 'content_type' => 'insight',
 											's' => get_search_query(),
+											// 'date_query' => array(
+											// 	array(
+											// 		'after'     => 'January 1st, 2022',
+											// 		'before'    => 'December 31st, 2023',
+											// 		'inclusive' => true,
+											// 	),
+											// ),
+											
+											
 										));
 										$postList = $postQuery->posts;
 										// $maxPages = $postQuery->max_num_pages;
@@ -101,9 +122,22 @@ global $wp_query;
 											'post_status'    => 'publish',
 											'fields' => 'ids',
 											'category_name' => 'events',
+											// 'orderby' => 'date',
+											'date_query' => array(
+												array(
+													'after'     => 'January 1st, 2022',
+													'before'    => 'December 31st, 2023',
+													'inclusive' => true,
+												),
+											),
+
+											
 											's' => get_search_query(),
 										));
+										
 										$postList = $postQuery->posts;
+
+
 										// $maxPages = $postQuery->max_num_pages;
 										// echo '<pre>'; print_r( $postList ); echo '</pre>';
 										if( count( $postList ) > 0 ):
@@ -127,8 +161,16 @@ global $wp_query;
 											'post_type'        => 'post',
 											'post_status'    => 'publish',
 											'fields' => 'ids',
-											'category_name' => 'news',
+											// 'category_name' => 'news',
+											'content_type' => 'news',
 											's' => get_search_query(),
+											'date_query' => array(
+												array(
+													'after'     => 'January 1st, 2022',
+													'before'    => 'December 31st, 2023',
+													'inclusive' => true,
+												),
+											)
 										));
 										$postList = $postQuery->posts;
 										// $maxPages = $postQuery->max_num_pages;
@@ -155,6 +197,13 @@ global $wp_query;
 											'post_status'    => 'publish',
 											'fields' => 'ids',											
 											's' => get_search_query(),
+											'date_query' => array(
+												array(
+													'after'     => 'January 1st, 2022',
+													'before'    => 'December 31st, 2023',
+													'inclusive' => true,
+												),
+											)
 										));
 										$postList = $postQuery->posts;
 										// $maxPages = $postQuery->max_num_pages;
